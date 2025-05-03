34046ed3f6e149dfb7e6edd03978f0f30e6974c5
NastyaParnet <nastya.parnet.01@gmail.com>
2025-05-03 09:36:23 +0300

    add mui

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import Styledroot from "./Styledroot";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <AppRouterCacheProvider options={{ key: "css" }}>
+          <Styledroot>{children}</Styledroot>
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+}
